c880817f3664430ba3a6860f212d547c9aa0123a
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-05-30 16:13:37 +0200

    add: join queries 3 and 4

--- MySQL join queries/queries.sql
+++ MySQL join queries/queries.sql
@@ -14,8 +14,23 @@ WHERE `departments`.`name` = "Dipartimento di Neuroscienze"
 AND `degrees`.`level` = "magistrale";
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `courses`.`id`, `courses`.`name` AS course_name, `courses`.`description`, `teachers`.`name` AS teacher_name, `teachers`.`surname` AS teacher_surname
+FROM `courses`
+INNER JOIN `course_teacher`
+ON `course_teacher`.`course_id` = `courses`.`id`
+INNER JOIN `teachers`
+ON `course_teacher`.`teacher_id` = `teachers`.`id`
+WHERE `teachers`.`id` = 44;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT `students`.`registration_number` AS student_code, `students`.`name` AS student_name, `students`.`surname` AS student_surname, `degrees`.*, `departments`.*
+FROM `students`
+INNER JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+INNER JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `student_name`, `student_surname`;
+
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
